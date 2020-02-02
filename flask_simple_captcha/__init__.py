39e6e87ec39abef296c5462ef1a431020484c8f8
root <ccarterdev@gmail.com>
2020-02-02 05:01:32 +0000

    basic functionality added

--- flask_simple_captcha/__init__.py
+++ flask_simple_captcha/__init__.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+from werkzeug.security import generate_password_hash, check_password_hash
+import base64
 import string
 import random
+
+
+HTML_ELEM_NAME = 'simplecaptcha'
+SECRET_CAPTCHA_KEY = 'CHANGEME - 40 or 50 character long key here'
+METHOD = 'pbkdf2:sha256:100'
+
+
+def random_string(min=40, max=50):
+    chars = string.ascii_uppercase
+    chars = chars + string.ascii_lowercase
+    chars = chars + string.digits
+
+    ran_int = random.randint(min, max)
+    return [random.choice(chars) for i in range(ran_int)]
 
 
 def get_background(text_size):
@@ -16,15 +33,13 @@ def draw_lines(im, lines=10):
     draw = ImageDraw.Draw(im)
 
     for i in range(lines):
-        print(im.size)
-
         if random.randint(0, 1) == 0:
             draw.line(
                      (random.randint(0, im.size[0]),
                       0,
                       random.randint(0, im.size[0]),
                       im.size[1],),
-                vwidth=2)
+                width=2)
         else:
             draw.ellipse(
                         (random.randint(-80, im.size[0]),
@@ -35,7 +50,7 @@ def draw_lines(im, lines=10):
     return im
 
 
-def create_captcha(length=5, digits=False):
+def create(length=5, digits=False):
     size = 30
     width, height = length * size, size
 
@@ -67,10 +82,61 @@ def create_captcha(length=5, digits=False):
 
     out = background.resize((200, 60))
     out = draw_lines(out)
-    out.save('a.png')
+    #out.save('a.png')
+
+    c_key = text + SECRET_CAPTCHA_KEY
+
+    c_hash = generate_password_hash(c_key,
+                                    method=METHOD,
+                                    salt_length=8)
+    c_hash = c_hash.replace(METHOD + '$', '')
+
+    return {'img': convert_b64img(out), 'text': text, 'hash': c_hash}
+
+
+def convert_b64img(out):
+    byte_array = BytesIO()
+    out.save(byte_array, format='PNG')
+    byte_array = byte_array.getvalue()
+
+    b64image = base64.b64encode(byte_array)
+    b64image = str(b64image)
+    b64image = b64image[2:][:-1]
+
+    return b64image
+
+
+def html(captcha):
+    img = '<img src="data:image/png;base64, %s />' % captcha['img']
+
+    inpu = '<input type="text" name="captcha-text">\n' + \
+           '<input type="hidden" name="captcha-hash" ' + \
+           'value="%s">' % captcha['hash']
+
+    return '%s\n%s' % (img, inpu)
+
+
+def verify(c_text, c_hash, c_key=SECRET_CAPTCHA_KEY):
+    c_text = c_text.upper()
+    c_hash = METHOD + '$' + c_hash
+    c_key = c_text + c_key
+    return check_password_hash(c_hash, c_key)
+
+
+def init_app(app):
+    app.jinja_env.globals.update(csrf_html=csrf_html)
+
+    if 'SECRET_CAPTCHA_KEY' in app.config.keys():
+        SECRET_CAPTCHA_KEY = app.config['SECRET_CAPTCHA_KEY']
+    else:
+        print('SECRET_CAPTCHA_KEY does not exist in app config.' +
+              'You should not use the default.')
+
+    return app
+
 
 def main():
-    create_captcha(5, True)
+    print(create())
 
 if __name__ == '__main__':
     main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as r:
+    README = r.read()
+
+setup(
+    name='flask-simple-captcha',
+    version='1.0.0',
+    description='Extremely simple, "Good Enough" captcha implemention for flask forms.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cc-d/flask-simple-captcha',
+    author='Cary Carter',
+    author_email='ccarterdev@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    install_requires=['Werkzeug==0.16.0', 'Pillow==6.1.0']
+)
