45511667ded3748156065be2b11a47ac2f534533
Cary Carter <ccarterdev@gmail.com>
2024-06-14 19:13:39 -0700

    updated debug server support for multiprocessing plus arbritrary captcha generation amoutn updated docs etc

--- debug_flask_server.py
+++ debug_flask_server.py
@@ -1,5 +1,6 @@
 import sys
 import os.path as op
+import multiprocessing as mp
 from flask import Flask, request, render_template_string
 
 from flask_simple_captcha import CAPTCHA, DEFAULT_CONFIG
@@ -9,6 +10,12 @@ test_config = DEFAULT_CONFIG.copy()
 
 CAPTCHA = CAPTCHA(config=test_config)
 app = CAPTCHA.init_app(app)
+
+PROCS = mp.cpu_count()
+
+
+def _captcha(mp_list: list):
+    mp_list.append(CAPTCHA.create())
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -30,9 +37,16 @@ def submit_captcha():
 
 
 @app.route('/images')
-def bulk_captchas():
-    num = 50
-    captchas = [CAPTCHA.create() for _ in range(num)]
+@app.route('/images/<int:captchas>')
+def bulk_captchas(captchas=None):
+    captchas = captchas or 50
+    with mp.Manager() as mgr:
+        mp_list = mgr.list()
+        with mp.Pool(PROCS) as pool:
+            pool.map(_captcha, [mp_list] * captchas)
+
+        captchas = list(mp_list)
+
     mimetype = 'image/png' if CAPTCHA.img_format == 'PNG' else 'image/jpeg'
     captchas = [
         '<img class="simple-captcha-img" '
